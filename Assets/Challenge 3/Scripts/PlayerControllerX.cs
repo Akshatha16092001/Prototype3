5f2619173d60d81db752c5bf8d58b6aa3b3f3503
Akshatha16092001 <akshatha.poojari@indivillage.co>
2025-10-29 14:23:03 +0530

    floating ballon in the air

--- Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -4,62 +4,68 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public bool gameOver;
+    public bool gameOver = false;
 
-    public float floatForce;
-    private float gravityModifier = 1.5f;
+    [Header("Movement Settings")]
+    public float forwardSpeed = 5f;   // moves along X-axis
+    public float floatForce = 8f;     // upward boost
+    public float hoverHeight = 6f;    // natural float height
+    public float hoverSmooth = 2f;    // smoothness of float correction
+    public float upperLimit = 15f;    // max height
+
     private Rigidbody playerRb;
 
+    [Header("Audio & Effects")]
     public ParticleSystem explosionParticle;
     public ParticleSystem fireworksParticle;
-
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        Physics.gravity *= gravityModifier;
+        playerRb = GetComponent<Rigidbody>();
         playerAudio = GetComponent<AudioSource>();
 
-        // Apply a small upward force at the start of the game
-        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        // No gravity – we control floating manually
+        playerRb.useGravity = false;
 
+        // Start above the ground
+        transform.position = new Vector3(0, hoverHeight, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (gameOver) return;
+
+        // Constant forward motion (X-axis)
+        transform.Translate(Vector3.right * forwardSpeed * Time.deltaTime, Space.World);
+
+        // Maintain floating height automatically
+        float heightError = hoverHeight - transform.position.y;
+        playerRb.AddForce(Vector3.up * heightError * hoverSmooth, ForceMode.Acceleration);
+
+        // Rise higher when space is held
+        if (Input.GetKey(KeyCode.Space) && transform.position.y < upperLimit)
         {
-            playerRb.AddForce(Vector3.up * floatForce);
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Acceleration);
         }
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        // if player collides with bomb, explode and set gameOver to true
         if (other.gameObject.CompareTag("Bomb"))
         {
             explosionParticle.Play();
             playerAudio.PlayOneShot(explodeSound, 1.0f);
             gameOver = true;
-            Debug.Log("Game Over!");
             Destroy(other.gameObject);
-        } 
-
-        // if player collides with money, fireworks
+        }
         else if (other.gameObject.CompareTag("Money"))
         {
             fireworksParticle.Play();
             playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
-
         }
-
     }
-
 }
--- Assets/Challenge 3/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 3/Scripts/SpawnManagerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] objectPrefabs;
-    private float spawnDelay = 2;
+    private float spawnDelay = 2f;
     private float spawnInterval = 1.5f;
 
     private PlayerControllerX playerControllerScript;
@@ -13,22 +13,20 @@ public class SpawnManagerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("PrawnsObject", spawnDelay, spawnInterval);
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+        InvokeRepeating("SpawnObjects", spawnDelay, spawnInterval);
     }
 
-    // Spawn obstacles
-    void SpawnObjects ()
+    // Spawn random objects while game is active
+    void SpawnObjects()
     {
-        // Set random spawn location and random object index
+        // Random spawn position in the air
         Vector3 spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
         int index = Random.Range(0, objectPrefabs.Length);
 
-        // If game is still active, spawn new object
         if (!playerControllerScript.gameOver)
         {
             Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
         }
-
     }
 }
